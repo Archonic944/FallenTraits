6c70be3bb6bfbc483c27233bb8c133cc4942af04
Archonic <gabrielc2904@gmail.com>
2022-01-04 18:31:20 -0500

    Converted to maven

---
+++ src/main/java/me/gabriel/Traits/Events.java
@@ -0,0 +1,40 @@
+package me.gabriel.Traits;
+
+import me.gabriel.Traits.data.TraitsData;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.Plugin;
+
+public class Events implements Listener {
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void forHSetAndSset(PlayerJoinEvent event) {
+		Player player = event.getPlayer();
+		forHSetAndSset(player);
+	}
+
+	public void forHSetAndSset(Player player) {
+		TraitsData data = TraitsData.get(player);
+		int hBonus = TraitsData.bonus(data.getHealthLevel());
+		int sBonus = TraitsData.bonus(data.getSpeedLevel());
+		player.setMaxHealth(0.2 * hBonus + 20);
+		player.setHealth(player.getMaxHealth());
+		player.setWalkSpeed((0.2f / 100) * sBonus + 0.2f);
+	}
+
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void forDamageAndDefense(EntityDamageByEntityEvent event) {
+		 if (event.getDamager() instanceof Player && event.getEntity() instanceof Player) {
+			 Player damager = (Player) event.getDamager();
+			 Player damagee = (Player) event.getEntity();
+			 TraitsData damagerData = TraitsData.get(damager);
+			 TraitsData damageeData = TraitsData.get(damagee);
+			 event.setDamage((event.getDamage()/100) * TraitsData.bonus(damagerData.getAttackLevel()) + event.getDamage() - ((event.getDamage()/100) * TraitsData.bonus(damageeData.getDefenseLevel())));
+		 }
+	}
+}
