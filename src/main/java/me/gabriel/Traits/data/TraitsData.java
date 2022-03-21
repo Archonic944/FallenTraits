5fa17371b875827186eb6e367aa243be952a54c2
Archonic <gabrielc2904@gmail.com>
2022-03-21 16:15:53 -0400

    Fixed excessive speed adding

--- src/main/java/me/gabriel/Traits/data/TraitsData.java
+++ src/main/java/me/gabriel/Traits/data/TraitsData.java
@@ -8,7 +8,7 @@ import org.bukkit.entity.Player;
 import java.util.UUID;
 
 public class TraitsData {
-    public static final float SPEED_TICK = 2f / 100;
+    public static final float SPEED_TICK = 0.2f / 100;
     public static final int BONUS_FACTOR = 2;
     @BsonProperty("defense")
     int defenseLevel = 0;
